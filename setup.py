8f69906634a8cf4c6224f182a95286767401be9e
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2021-01-31 20:52:45 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ciboulette-pkg-Touzan-Dominique", # Replace with your own username
+    version="0.0.1",
+    author="Touzan Dominique",
+    author_email="dtouzan@gmail.com",
+    description="Ciboulette package for astronomy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
