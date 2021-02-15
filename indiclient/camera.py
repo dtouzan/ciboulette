71fa54ef2e10b024956743692f23735a67dd32e2
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2021-02-16 00:13:18 +0100

    modification 16/02/2021

--- indiclient/camera.py
+++ indiclient/camera.py
@@ -46,3 +46,75 @@ class ASICam120Mini(CCDCam):
         gain = self.get_float(self.driver, "CCD_CONTROLS", "Gain")
         return gain
 
+class ATIKCam383L(CCDCam):
+    """
+    Wrap CCDCam, set driver to ATIK 383L+ CCD, and point to localhost by default.
+    """
+    def __init__(self, host='localhost', port=7624):
+        super(ATIKCam383L, self).__init__(host, port, driver="Atik 383L")
+        self.camera_name = "Atik"
+        self.process_events()
+
+    @property
+    def filters(self):
+        return ["N/A"]
+
+    @property
+    def filter(self):
+        return "N/A"
+
+    @filter.setter
+    def filter(self, f):
+        pass
+
+    def local(self):
+        """
+        Enable local write
+        """
+        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "UPLOAD_MODE", "Local")
+        if self.debug:
+            vec.tell()
+        return vec
+
+    def client(self):
+        """
+        Enable client write
+        """
+        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "UPLOAD_MODE", "Client")
+        if self.debug:
+            vec.tell()
+        return vec
+
+    def both(self):
+        """
+        Enable both write
+        """
+        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "UPLOAD_MODE", "Both")
+        if self.debug:
+            vec.tell()
+        return vec
+
+ 
+    @property
+    def updir(self):
+        upload_dir = self.get_text(self.driver, "UPLOAD_SETTINGS", "UPLOAD_DIR")
+        return upload_dir
+
+    @updir.setter
+    def updir(self,string):
+        """
+        Configure UPLOAD_DIR : IMAGE_XXX
+        """
+        self.set_and_send_text(self.driver, "UPLOAD_SETTINGS", "UPLOAD_DIR", string)
+
+    @property
+    def prefix(self):
+        upload_prefix = self.get_text(self.driver, "UPLOAD_SETTINGS", "UPLOAD_PREFIX")
+        return upload_prefix      
+        
+    @prefix.setter
+    def prefix(self,string):
+        """
+        Configure UPLOAD_PREFIX : IMAGE_XXX
+        """
+        self.set_and_send_text(self.driver, "UPLOAD_SETTINGS", "UPLOAD_PREFIX", string)
