9ec0b76bc79f7f25ab666776d52796d9b740fd0e
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2021-04-10 23:06:56 +0200

    update indicam class

--- base/ciboulette.py
+++ base/ciboulette.py
@@ -421,10 +421,6 @@ class Ciboulette(object):
             fits.writeto(file_name, hdu.data, hdu.header, overwrite=True)  
         else:
             hdul = camera.startexposure(self._exp_time,True)
-            #i = self._exp_time + ((self.naxis1 * self.naxis2)/1048576)
-            #while i > 0:
-                #time.sleep(1)
-                #i = i - 1
             file_name = self.dataset+'/_'+self.observer_name+'_'+self.object_name+'_'+str(self._frameid)+'.fits'
             fits.writeto(file_name, hdul[0].data, hdul[0].header, overwrite=True)  
         self.extendedfits()           
