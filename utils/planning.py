9b8496287cef0657d8130d1ae2b2ec69db5af386
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2021-05-10 22:31:44 +0200

    update planning class

--- utils/planning.py
+++ utils/planning.py
@@ -17,13 +17,12 @@ class Planning(object):
     """
     
     def __init__(self,idgoogledrive='1Yc-QxFr9veMeGjqvedRMrcEDL2GRyTS_',fileoutput='planning.csv'):       
-        self.data = []
         self.idgoogledrive = idgoogledrive
         self.fileoutput = fileoutput
     
 
     @property
-    def get(self):
+    def read(self):
         """
         Return table of planning with google drive 
          Ex:  wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Yc-QxFr9veMeGjqvedRMrcEDL2GRyTS_' -O planning.csv        
@@ -71,40 +70,32 @@ class Planning(object):
         """
         Return filter name of plan
          plan (Table): plan of planning.
-        """
-        filter_name = 'L'
-        filter_name = plan[constent.MAST_filters]       
-        return filter_name
+        """    
+        return plan[constent.MAST_filters] 
     
 
     def ra(self,plan):
         """
         Return RA of plan. Format: Hours H.HHHH
          plan (Table): plan of planning.
-        """     
-        ra = 0.0
-        ra = plan[constent.MAST_s_ra]        
-        return ra
+        """           
+        return plan[constent.MAST_s_ra]
 
 
     def dec(self,plan):
         """
         Return RA of plan. Format: Degrees D.DDDD
          plan (Table): plan of planning.
-        """
-        dec = 0.0
-        dec = plan[constent.MAST_s_dec]     
-        return dec
+        """    
+        return plan[constent.MAST_s_dec] 
 
 
     def observationID(self,plan):
         """
         Return observation ID of plan
          plan (Table): plan of planning.
-        """
-        obs_id = 0
-        obs_id = plan[constent.MAST_obs_id]         
-        return obs_id
+        """        
+        return plan[constent.MAST_obs_id]
     
 
     def exptime(self,plan):
@@ -112,19 +103,15 @@ class Planning(object):
         Return exposure of plan
          plan (Table): plan of planning.
         """    
-        exptime = 0
-        exptime = plan[constent.MAST_t_exptime] 
-        return exptime
+        return plan[constent.MAST_t_exptime]
     
 
-    def observaiontitle(self,plan):
+    def observationtitle(self,plan):
         """
         Return observaion title of plan
          plan (Table): plan of planning.
         """       
-        obs_title = 'none'
-        obs_title = plan[constent.MAST_obs_title] 
-        return obs_title
+        return plan[constent.MAST_obs_title] 
     
 
     def dataproducttype(self,plan):
@@ -132,6 +119,11 @@ class Planning(object):
         Return dataproduct type of plan
          plan (table): plan of planning.          
         """       
-        dataproduct_type = 'Intensity'
-        dataproduct_type = plan[constent.MAST_dataproduct_type] 
-        return dataproduct_type
+        return plan[constent.MAST_dataproduct_type]
+
+    def target(self,plan):
+        """
+        Return target of plan
+         plan (table): plan of planning.          
+        """       
+        return plan[constent.MAST_target_name]
