e76bb9be9e5ce15cb66ecdd58d8050a8c1d22aa6
Dominique Touzan <dtouzan@gmail.com>
2021-08-05 22:00:41 +0200

    update archive class

--- utils/archive.py
+++ utils/archive.py
@@ -6,35 +6,48 @@ Archive class
 class Archive(object):
     
     def __init__(self, archive_table = 'dataset/archives'):
-        self.archive_table = archive_table
+        self.directory = archive_table
+        self.dataset = []       
      
     @property
-    def readarchives(self):
+    def read(self):
         """
         Return table of archive of the fits files in the directory      
         """
         object_name = []
+        observer = []
+        instrument = []
+        telescope = []
         frameid = []
+        frame = []
         datatype = []
-        RA = []
-        DEC = []    
-        arch = os.listdir(self.archive_table)   
+        filter_name = []
+        focal =[]
+        exptime =[]
+        date_obs =[]
+        jd = []
+        ra = []
+        dec = []    
+        arch = os.listdir(self.self.directory)   
         for file in arch:
-            fits_file = get_pkg_data_filename(data_arch+'/'+file)
+            fits_file = get_pkg_data_filename(self.directory)
             hdu = fits.open(fits_file)[0]
-            header = hdu.header
-              
+            header = hdu.header             
             object_name.append(header['OBJECT'])
+            observer.append(header['OBSERVER'])     
+            instrument.append(header['INSTRUME']) 
+            telescope.append(header['TELESCOP'])
             frameid.append(header['FRAMEID'])
+            frame.append(header['FRAME'])
             datatype.append(header['DATATYPE'])
-            RA.append(header['CRVAL1'])
-            DEC.append(header['CRVAL2'] )    
-            
-        return Table([object_name,frameid,datatype,RA,DEC], names=['ID','FRAMEID','DATATYPE','RA','DEC'])
+            filter_name.append(header['FILTER'])
+            focal.append(header['FOCALLEN'])
+            exptime.append(header['EXPTIME'])
+            date_obs.append(header['DATE-OBS'])
+            jd.append(header['JD-OBS'])
+            ra.append(header['CRVAL1'])
+            dec.append(header['CRVAL2'] )    
+             
+        self.dataset = Table([object_name,observer,instrument,telescope,frameid,frame,datatype,filter_name,focal,exptime,date_obs,jd,ra,dec], 
+                             names=['ID','OBSERVER','INSTRUME','TELESCOP','FRAMEID','FRAME','DATATYPE','FILTER','FOCALLEN','EXPTIME','DATE-OBS','JD-OBS','RA','DEC'])
 
-    @property
-    def MASTcreate(self)
-        """
-        Create MAST type file with fits archives file
-        """
-        return True
