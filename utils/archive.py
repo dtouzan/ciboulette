bde667fe91d8bc8eb5567a23010fb406f103cf43
Dominique Touzan <dtouzan@gmail.com>
2021-08-07 22:32:53 +0200

    update archive class add plot function

--- utils/archive.py
+++ utils/archive.py
@@ -2,17 +2,27 @@
 Archive class
 """
 
+import os
+import matplotlib.pyplot as plt
+from astropy.table import Table
+from astropy import units as u
+from astropy.time import Time
+from astropy.io import fits
+from astropy.utils.data import get_pkg_data_filename
 
 class Archive(object):
     
     def __init__(self, archive_table = 'dataset/archives'):
         self.directory = archive_table
-        self.dataset = []       
+        self.dataset = []
+        self.go = 2017
+        self.end = 2021
      
     @property
     def read(self):
         """
-        Return table of archive of the fits files in the directory      
+        Return table of archive of the fits files in the directory
+        Format fits or OBJECT-AAAAMMDD-HHMM-EXPOSTIME-FOCAL.fits
         """
         object_name = []
         observer = []
@@ -27,30 +37,173 @@ class Archive(object):
         date_obs =[]
         jd = []
         ra = []
-        dec = []    
-        arch = os.listdir(self.self.directory)   
+        dec = [] 
+        filename = []
+        arch = os.listdir(self.directory)   
         for file in arch:
-            fits_file = get_pkg_data_filename(self.directory)
-            hdu = fits.open(fits_file)[0]
-            header = hdu.header             
-            object_name.append(header['OBJECT'])
-            observer.append(header['OBSERVER'])     
-            instrument.append(header['INSTRUME']) 
-            telescope.append(header['TELESCOP'])
-            frameid.append(header['FRAMEID'])
-            frame.append(header['FRAME'])
-            datatype.append(header['DATATYPE'])
-            filter_name.append(header['FILTER'])
-            focal.append(header['FOCALLEN'])
-            exptime.append(header['EXPTIME'])
-            date_obs.append(header['DATE-OBS'])
-            jd.append(header['JD-OBS'])
-            ra.append(header['CRVAL1'])
-            dec.append(header['CRVAL2'] )    
+            if '.fit' in file:
+                hdu = fits.open(self.directory+'/'+file)[0]
+                header = hdu.header   
+                
+                object_name.append(self.object_name(file))
+                 
+                if 'OBSERVER' in header:
+                    observer.append(header['OBSERVER'])    
+                else:
+                    observer.append('Nan')    
+                    
+                if 'INSTRUME' in header:
+                    instrument.append(header['INSTRUME']) 
+                else:
+                    instrument.append('Nan') 
+                
+                if 'TELESCOP' in header:
+                    telescope.append(header['TELESCOP'])
+                else:
+                    telescope.append('Nan')
+                
+                if 'FRAMEID' in header:
+                    frameid.append(header['FRAMEID'])
+                else:
+                    frameid.append('Nan')
+                
+                if 'FRAME' in header:
+                    frame.append(header['FRAME'])
+                else:
+                    frame.append('Nan')
+                
+                if 'DATATYPE' in header:
+                    datatype.append(header['DATATYPE'])
+                else:
+                    datatype.append('Nan')
+                
+                if 'FILTERS' in header:
+                    filter_name.append(header['FILTERS'])
+                else:
+                    if 'FILTER' in header:
+                        filter_name.append(header['FILTER'])
+                    else:
+                        filter_name.append('Nan')
+                
+                if 'FOCALLEN' in header:
+                    focal.append(header['FOCALLEN'])
+                else:
+                    focal.append(self.focal(file))
+                
+                if 'DATE-OBS' in header:
+                    date_obs.append(header['DATE-OBS'])
+                else:
+                    date_obs.append(self.date(file))   
+                                  
+                if 'JD-OBS' in header:
+                    jd.append(header['JD-OBS'])
+                else:          
+                    if 'JD' in header:
+                        jd.append(header['JD'])
+                    else:
+                        if 'DATE-OBS' in header:
+                            jd.append(self.JulianDay(header['DATE-OBS']))
+                        else:
+                            jd.append(self.JulianDay(self.date(file)))
+                
+                if 'x' in file:
+                    exptime.append(self.exptime(file))
+                else:
+                    exptime.append(header['EXPTIME'])
+                
+                if 'RA' in header:
+                    ra.append(header['RA'])
+                else:
+                    ra.append('Nan')
+                    
+                if 'DEC' in header:    
+                    dec.append(header['DEC']) 
+                else:
+                    dec.append('Nan') 
+                    
+                filename.append(file.replace('.fits',''))
              
-        self.dataset = Table([object_name,observer,instrument,telescope,frameid,frame,datatype,filter_name,focal,exptime,date_obs,jd,ra,dec], 
-                             names=['ID','OBSERVER','INSTRUME','TELESCOP','FRAMEID','FRAME','DATATYPE','FILTER','FOCALLEN','EXPTIME','DATE-OBS','JD-OBS','RA','DEC'])
+        self.dataset = Table([object_name,observer,instrument,telescope,frameid,frame,datatype,filter_name,focal,exptime,date_obs,jd,ra,dec,filename], 
+                             names=['ID','OBSERVER','INSTRUME','TELESCOP','FRAMEID','FRAME','DATATYPE','FILTER','FOCALLEN','EXPTIME','DATE-OBS','JD','RA','DEC','FILE'])
 
     @property
     def observations(self):
+        """
+        Return header of list files
+        """
         return self.dataset
+    
+    def JulianDay(self,string):
+        """
+        Return JD od DATE
+        """
+        time = Time(string)
+        return time.jd
+    
+    def object_name(self,string):
+        """
+        Return object name of first word of filename
+        """    
+        return string.split('-')[0]
+    
+    def focal(self,string):
+        """
+        Return focal of last word of filename
+        """   
+        return string.replace('.fits','').split('-')[4].replace('f','')
+
+    def date(self,string):
+        """
+        Return date end time of word of filename
+        """  
+        d = string.replace('.fits','').split('-')[1]
+        t = string.replace('.fits','').split('-')[2]
+        d = d[:4] + '-' + d[4:6] + '-' + d[6:8] + 'T' + t[:2] + ':' + t[2:4] + ':00'
+        return d                  
+ 
+    def exptime(self,string):      
+        """
+        Return exposition time of word of filename
+        """  
+        exp = string.replace('.fits','').split('-')[3].replace('s','')
+        number = float(exp.split('x')[0])
+        time = float(exp.split('x')[1])
+        e = number * time
+        return e             
+        
+    def header(self,filename):
+        """
+        Return header of file with index line
+        """
+        hdu = fits.open(self.directory+'/'+filename+'.fits')[0]
+        return hdu.header 
+    
+    @property
+    def period(self):
+        """
+        Return period of base
+        """
+        return max(self.dataset['JD'])-min(self.dataset['JD'])
+    
+    @property
+    def exptimes(self):
+        """
+        Return period of base
+        """
+        return sum(self.dataset['EXPTIME'])
+    
+    def plot(self):
+        """
+        Plot JD planning
+        """
+        years_data = []
+        for years in range(self.go,self.end+1):
+            date = str(years)+'-01-01T00:00:00'
+            years_data.append(self.JulianDay(date))
+            
+        plt.ylim(0,1)
+        plt.xlim(min(years_data),max(years_data))
+        plt.vlines(years_data, 0,1 , colors = 'red', alpha = 0.1, lw=15)
+        plt.vlines(self.dataset['JD'], 0,1 , colors = 'black', alpha = 0.3)
+
+        plt.show()
