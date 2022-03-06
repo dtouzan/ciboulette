b0725c4c2b7d4d63fce1d2236748addc5e2bd9d6
Dominique Touzan <dtouzan@gmail.com>
2022-03-06 13:23:22 +0100

    Create indi-3rdparty

---
+++ indiclient/indi-3rdparty/indi-sa200/build/CMakeFiles/indi_sa200.dir/.ipynb_checkpoints/cmake_clean-checkpoint.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/indi_sa200.dir/indi_sa200.cpp.o"
+  "indi_sa200"
+  "indi_sa200.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indi_sa200.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
