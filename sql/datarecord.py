c3a34f0011931b8d5a0600fecfb3de7d88066eeb
Dominique Touzan <dtouzan@gmail.com>
2024-03-14 23:15:54 +0100

    Update datarecord.py

--- sql/datarecord.py
+++ sql/datarecord.py
@@ -1,5 +1,7 @@
 """
-class SQLite3 select for archivage, Mast modules
+class data record for SQL interface
+
+IN PROGRESS, DO NOT USED
 """
 
 __authors__ = ("Dominique Touzan","ciboulette module")
@@ -13,6 +15,7 @@ from ciboulette.sql import compoments, interface
 
 class datarecords(interface.interfaces):
     filter_names = ['scienceprogram', 'observation', 'instrument', 'target', 'sequence', 'ObservingConditions', 'Observinglog']
+
 # faire class datarecord pour la création d'une observation complète (héritage interface)
 # datarecord.edit visualise les infos en fonction du filtre
 # datarecord.filter paramettrer le filtre de recherche (target,observation,scienprogram....)
@@ -23,20 +26,30 @@ class datarecords(interface.interfaces):
 # calcul du champ en recherchant et en prenant INSTRUMENT/CAMERA (à rechercher dans TABLE/camera)    
     def __init__(self):
         self.filters = datarecords.filter_names[1]
-        self.value = '' # String 
+        self.header = 'id'
+        self.values = '1'
     
     @property
     def edit(self):
         """
         Print observation text type
         """
-        if type(self.value) == 'string' or type(self.value) != '':
+        if type(self.values) == 'string' or type(self.values) != '':
             
             if self.filters == datarecords.filter_names[1]:
-                self.observation_print(int(self.value))
+                if self.header == 'id':
+                    self.observation_print(int(self.values))
+                if self.header == 'title':
+                    #self.observation_print(int(self.values))
+                    print(self.values)
+                if self.header == 'scheduling':
+                    #self.observation_print(int(self.values))
+                    print(self.values)
         
             if self.filters == datarecords.filter_names[3]:
-                for data in self.target_by_name(self.value):
-                    self.observation_print(data['observation_id'])
+                if self.header == 'name':
+                    for data in self.target_by_name(self.values):
+                        self.observation_print(data['observation_id'])
 
-
+            #INSTRUMENT/NAME,FILTER,DISPERSER,CAMERA
+            #OBSERVATION,TITLE,SCHEDULING
