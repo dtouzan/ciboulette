63907c6270e341339aeb7de1d50ca0044ed567f2
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2025-07-20 17:34:13 +0200

    Delete EAFpy

--- indiclient/indi_EAFpy_Focuser/indi_EAFpy_focuser.xml.cmake
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<driversList>
-   <devGroup group="Focusers">
-      <device label="EAFpy Focuser" manufacturer="indi-dev-tutorials">
-         <driver name="EAFpy Focuser">indi_EAFpy_focuser</driver>
-         <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
-      </device>
-   </devGroup>
-</driversList>
