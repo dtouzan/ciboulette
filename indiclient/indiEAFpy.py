495910d73a31bfad772420b9cd13343bdce6b4c4
Touzan Dominique <51866193+dtouzan@users.noreply.github.com>
2025-08-06 23:26:44 +0200

    Update focuser

--- indiclient/indiEAFpy.py
+++ indiclient/indiEAFpy.py
@@ -5,7 +5,34 @@
 Classes and utility functions for communicating with cameras via the INDI protocol, http://www.indilib.org.
 
 # indi_EAFpy_focuser
-
+EAFpy_Focuser.CONNECTION.CONNECT=On
+EAFpy_Focuser.CONNECTION.DISCONNECT=Off
+EAFpy_Focuser.DRIVER_INFO.DRIVER_NAME=EAFpy_Focuser
+EAFpy_Focuser.DRIVER_INFO.DRIVER_EXEC=indi_EAFpy_focuser
+EAFpy_Focuser.DRIVER_INFO.DRIVER_VERSION=1.2
+EAFpy_Focuser.DRIVER_INFO.DRIVER_INTERFACE=8
+EAFpy_Focuser.DEBUG.ENABLE=Off
+EAFpy_Focuser.DEBUG.DISABLE=On
+EAFpy_Focuser.POLLING_PERIOD.PERIOD_MS=1000
+EAFpy_Focuser.SIMULATION.ENABLE=Off
+EAFpy_Focuser.SIMULATION.DISABLE=On
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_LOAD=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_SAVE=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_DEFAULT=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_PURGE=Off
+EAFpy_Focuser.FOCUS_MOTION.FOCUS_INWARD=On
+EAFpy_Focuser.FOCUS_MOTION.FOCUS_OUTWARD=Off
+EAFpy_Focuser.REL_FOCUS_POSITION.FOCUS_RELATIVE_POSITION=50
+EAFpy_Focuser.FOCUS_ABORT_MOTION.ABORT=Off
+EAFpy_Focuser.USEJOYSTICK.ENABLE=Off
+EAFpy_Focuser.USEJOYSTICK.DISABLE=On
+EAFpy_Focuser.SNOOP_JOYSTICK.SNOOP_JOYSTICK_DEVICE=Joystick
+EAFpy_Focuser.FOCUS_SPEED_SP.FOCUS_SPEED_1=On
+EAFpy_Focuser.FOCUS_SPEED_SP.FOCUS_SPEED_2=Off
+EAFpy_Focuser.DELAY.DELAY_VALUE=10
+EAFpy_Focuser.USEJOYSTICK.ENABLE=Off
+EAFpy_Focuser.USEJOYSTICK.DISABLE=On
+EAFpy_Focuser.SNOOP_JOYSTICK.SNOOP_JOYSTICK_DEVICE=Joystick
 """
 
 __authors__ = ("Dominique Touzan","ciboulette module")
@@ -33,7 +60,7 @@ class EAFpy(Focuser):
     Wrap EAFpy,set the driver to the EAFpy driver 
     """
     def __init__(self, host="localhost", port=7624):
-        super(EAFpy, self).__init__(host, port, driver="indi_EAFpy_focuser")
+        super(EAFpy, self).__init__(host, port, driver="EAFpy_Focuser")
         self.focuser_name = "EAFpy"
 
     @property
@@ -45,47 +72,86 @@ class EAFpy(Focuser):
     @property
     def ticks(self):
         """
-        Retur indi_EAFpy_focuser_FOCUSER.TICKS
+        Return ticks and init 0
         """
+        value = self.get_text(self.driver, "REL_FOCUS_POSITION", "FOCUS_RELATIVE_POSITION")
+        return int(value)
 
     @ticks.setter
     def ticks(self,value=25):
         """
-        Set Ticks and go
+        Set Ticks with value 0<ticks<500
         """
+        if  0<= value <=500:
+            self.set_and_send_float(self.driver, 'REL_FOCUS_POSITION', 'FOCUS_RELATIVE_POSITION', value) 
+            self.process_events()
 
     @property
     def speed(self):
         """
         Return speed
         """
+        Speed_1 = self.get_text(self.driver, "FOCUS_SPEED_SP", "FOCUS_SPEED_1")
+        Speed_2 = self.get_text(self.driver, "FOCUS_SPEED_SP", "FOCUS_SPEED_2")
+        return Speed_1, Speed_2
 
-    @speed.setter
-    def speed(self,value=1):
+    @property
+    def speed1(self):
         """
         Set Speed
         """
+        self.set_and_send_text(self.driver, 'FOCUS_SPEED_SP', 'FOCUS_SPEED_2', 'Off')
+        self.set_and_send_text(self.driver, 'FOCUS_SPEED_SP', 'FOCUS_SPEED_1', 'On')
+    
+    @property
+    def speed2(self):
+        """
+        Set Speed
+        """
+        self.set_and_send_text(self.driver, 'FOCUS_SPEED_SP', 'FOCUS_SPEED_1', 'Off')
+        self.set_and_send_text(self.driver, 'FOCUS_SPEED_SP', 'FOCUS_SPEED_2', 'On')
+
         
     @property
-    def timer(self):
+    def delay(self):
         """
-        Return timer 
+        Return delay 
         """
+        self.process_events()
+        value = self.get_float(self.driver, "DELAY", "DELAY_VALUE")
+        return value
 
-    @timer.setter
-    def timer(self,value=10):
+    @delay.setter
+    def delay(self,value=10):
         """
-        Set timer
+        Set delay
         """
+        if  2<= value <=25:
+            self.set_and_send_float(self.driver, 'DELAY', 'DELAY_VALUE', value) 
+            self.process_events()
+            
     @property
     def direction(self):
         """
         Return direction 
         """
+        Focus_In = self.get_text(self.driver, "FOCUS_MOTION", "FOCUS_INWARD")
+        Focus_Out = self.get_text(self.driver, "FOCUS_MOTION", "FOCUS_OUTWARD")
+        return Focus_In, Focus_Out
 
-    @direction.setter
-    def direction(self,value=In):
+    @property
+    def inward(self):
         """
-        Set direction
+        Set inward
         """
-    
+        self.set_and_send_text(self.driver, 'FOCUS_MOTION', 'FOCUS_OUTWARD', 'Off')
+        self.set_and_send_text(self.driver, 'FOCUS_MOTION', 'FOCUS_INWARD', 'On')
+
+    @property
+    def outward(self):
+        """
+        Set outward
+        """
+        self.set_and_send_text(self.driver, 'FOCUS_MOTION', 'FOCUS_INWARD', 'Off')
+        self.set_and_send_text(self.driver, 'FOCUS_MOTION', 'FOCUS_OUTWARD', 'On')
+
--- indiclient/indifocuser.py
+++ indiclient/indifocuser.py
@@ -3,6 +3,36 @@
 
 """
 Classes and utility functions for communicating with mount a the INDI protocol, http://www.indilib.org.
+
+# indi_EAFpy_focuser
+EAFpy_Focuser.CONNECTION.CONNECT=On
+EAFpy_Focuser.CONNECTION.DISCONNECT=Off
+EAFpy_Focuser.DRIVER_INFO.DRIVER_NAME=EAFpy_Focuser
+EAFpy_Focuser.DRIVER_INFO.DRIVER_EXEC=indi_EAFpy_focuser
+EAFpy_Focuser.DRIVER_INFO.DRIVER_VERSION=1.2
+EAFpy_Focuser.DRIVER_INFO.DRIVER_INTERFACE=8
+EAFpy_Focuser.DEBUG.ENABLE=Off
+EAFpy_Focuser.DEBUG.DISABLE=On
+EAFpy_Focuser.POLLING_PERIOD.PERIOD_MS=1000
+EAFpy_Focuser.SIMULATION.ENABLE=Off
+EAFpy_Focuser.SIMULATION.DISABLE=On
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_LOAD=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_SAVE=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_DEFAULT=Off
+EAFpy_Focuser.CONFIG_PROCESS.CONFIG_PURGE=Off
+EAFpy_Focuser.FOCUS_MOTION.FOCUS_INWARD=On
+EAFpy_Focuser.FOCUS_MOTION.FOCUS_OUTWARD=Off
+EAFpy_Focuser.REL_FOCUS_POSITION.FOCUS_RELATIVE_POSITION=50
+EAFpy_Focuser.FOCUS_ABORT_MOTION.ABORT=Off
+EAFpy_Focuser.USEJOYSTICK.ENABLE=Off
+EAFpy_Focuser.USEJOYSTICK.DISABLE=On
+EAFpy_Focuser.SNOOP_JOYSTICK.SNOOP_JOYSTICK_DEVICE=Joystick
+EAFpy_Focuser.FOCUS_SPEED_SP.FOCUS_SPEED_1=On
+EAFpy_Focuser.FOCUS_SPEED_SP.FOCUS_SPEED_2=Off
+EAFpy_Focuser.DELAY.DELAY_VALUE=10
+EAFpy_Focuser.USEJOYSTICK.ENABLE=Off
+EAFpy_Focuser.USEJOYSTICK.DISABLE=On
+EAFpy_Focuser.SNOOP_JOYSTICK.SNOOP_JOYSTICK_DEVICE=Joystick
 """
 
 __authors__ = ("Dominique Touzan","ciboulette module")
@@ -31,8 +61,8 @@ class Focuser(indiclient):
     mount, etc.
     """
     def __init__(self, host, port, driver="Focuser simulator", debug=True):
-        super(Telescope, self).__init__(host, port)
-        self.mount_name = "Focuser Default"
+        super(Focuser, self).__init__(host, port)
+        self.focuser_name = "Focuser Default"
         self.driver = driver
         self.debug = debug
         if not self.connected:
@@ -57,7 +87,7 @@ class Focuser(indiclient):
 
     def connect(self):
         """
-        Enable mount connection
+        Enable focuser connection
         """
         vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CONNECTION", "Connect")
         if self.debug and vec is not None:
@@ -90,3 +120,17 @@ class Focuser(indiclient):
         vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "TELESCOPE_ABORT_MOTION", 'Abort')       
         if self.debug:
             vec.tell()
+
+    @property 
+    def load(self):      
+        """Load indilid configuration file .xml
+        """
+        self.set_and_send_text(self.driver, "CONFIG_PROCESS", "CONFIG_LOAD", "On")
+        return True
+
+    @property 
+    def save(self):      
+        """Save indilid configuration file .xml
+        """
+        self.set_and_send_text(self.driver, "CONFIG_PROCESS", "CONFIG_SAVE", "On")
+        return True 
